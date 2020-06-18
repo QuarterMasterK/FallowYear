52b7d9dae200aca094ba2b282362f164a320a2e5
Quelich <emre1kilic@outlook.com>
2020-06-18 23:50:20 +0300

    Add files via upload

---
+++ FallowYear.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class FallowYear {
+private static  Scanner scan = new Scanner(System.in);
+public static void main(String[] args) {
+    
+    System.out.println("Wide x Length");
+    int wide = scan.nextInt();
+    int length = scan.nextInt();
+    System.out.println("Initial Year: ");
+    int year = scan.nextInt();
+    int[][] firstYear = new int[wide][length]; // first year
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {firstYear[i][j] = 0;}
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int d = 0; d < 10; d++) {System.out.print(firstYear[i][d]+ " ");}
+        System.out.println(" ");
+    }
+
+    boolean flag = true;
+    while (flag) {
+        scan = new Scanner(System.in);
+        boolean EmptyChecker;
+        EmptyChecker = scan.hasNextInt();
+        if(EmptyChecker) {
+            System.out.println(year + 1);
+            int startx, starty, endx, endy;
+            startx = scan.nextInt();
+            starty = scan.nextInt();
+            endx = scan.nextInt();
+            endy = scan.nextInt();
+            for (int i = starty; i <= endy; i++) {
+                for (int j = startx; j <= endx; j++) {firstYear[i][j] += 2; }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (firstYear[i][j] > 0) {firstYear[i][j]--;}
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {System.out.print(firstYear[i][j]+ " ");}
+                System.out.println(" ");
+            }
+        }
+
+        else {
+            System.out.println(year + 1);
+            for (int i = 0; i < 10; i++) {
+                for (int r = 0; r < 10; r++) {if (firstYear[i][r] > 0) {firstYear[i][r]--;}}
+            }
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {System.out.print(firstYear[i][j]+ " ");}
+                System.out.println(" ");
+            }
+        }
+        year++;
+        if(year == 2020) break;
+        
+        }
+    }
+}
